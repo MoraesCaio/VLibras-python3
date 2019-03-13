b0efe6175d8dc676d0ce77898f3dbba7665e321e
MoraesCaio <caiomoraes.cesar@gmail.com>
2019-03-13 15:00:54 +0000

    Script instalação

--- vlibras-translate/scripts/linux/install.sh
+++ vlibras-translate/scripts/linux/install.sh
@@ -1,53 +1,57 @@
 #!/bin/bash
 
+
 read -r -p "Deseja fazer o download das dependências? [Y/n] " response
- response=${response,,} # tolower
- if [[ $response =~ ^(yes|y| ) ]]; then
-    mkdir ~/vlibras-libs
-	cd ~/vlibras-libs
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]]; then
+ 	cd ~
+	git clone https://github.com/MoraesCaio/VLibras-python3.git open-signs
+    # mkdir ~/open-signs
+	cd open-signs
 
-	mkdir aelius
-	cd aelius
+	# mkdir aelius
+	# cd aelius
 
-	wget 150.165.204.30:8080/translate/linux/aelius-install.tar.gz
+	# wget 150.165.204.30:8080/translate/linux/aelius-install.tar.gz
 
-	MACHINE_TYPE=`uname -m`
-	if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-		wget 150.165.204.30:8080/translate/linux/hunpos/x86_64/hunpos-tag
-	else
-		wget 150.165.204.30:8080/translate/linux/hunpos/i386/hunpos-tag
-	fi
+	# MACHINE_TYPE=`uname -m`
+	# if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+	# 	wget 150.165.204.30:8080/translate/linux/hunpos/x86_64/hunpos-tag
+	# else
+	# 	wget 150.165.204.30:8080/translate/linux/hunpos/i386/hunpos-tag
+	# fi
 
-     read -r -p "Deseja instalar as dependências? [Y/n] " response
-	 response=${response,,} # tolower
-	 if [[ $response =~ ^(yes|y| ) ]]; then
-	    echo -e "\n# Extraindo...\n"
-		tar -xf aelius-install.tar.gz -C .
-		mkdir bin
-		mv hunpos-tag bin/
-		chmod 777 bin/hunpos-tag
+	chmod 777 vlibras-libs/aelius/bin/hunpos-tag
+
+	read -r -p "Deseja instalar as dependências? [Y/n] " response
+	response=${response,,} # tolower
+	if [[ $response =~ ^(yes|y| ) ]]; then
+	    # echo -e "\n# Extraindo...\n"
+		# tar -xf aelius-install.tar.gz -C .
+		# mkdir bin
+		# mv hunpos-tag bin/
 
 		echo -e "# Instalando dependências...\n"
 		sudo apt-get update
 		sudo apt-get install -y python-dev python-setuptools python-pip python-flask python-yaml python-numpy python-matplotlib
-		sudo pip install nltk==3.0.5 flask-cors nltk_tgrep --upgrade
+		pip3 install nltk flask-cors nltk_tgrep --upgrade
 
-	 fi
+	fi
 
-	 read -r -p "Deseja limpar os arquivos temporários? [Y/n] " response
-	 response=${response,,} # tolower
-	 if [[ $response =~ ^(yes|y| ) ]]; then
-	    rm ~/vlibras-libs/aelius/aelius-install.tar.gz
-	 fi
+	# read -r -p "Deseja limpar os arquivos temporários? [Y/n] " response
+	# response=${response,,} # tolower
+	# if [[ $response =~ ^(yes|y| ) ]]; then
+	# 	rm ~/vlibras-libs/aelius/aelius-install.tar.gz
+	# fi
 
- fi
+fi
 
 
 read -r -p "Deseja criar as variáveis de ambiente? [Y/n] " response
- response=${response,,} # tolower
- if [[ $response =~ ^(yes|y| ) ]]; then
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]]; then
 
-    	# path to Hunpos Tag
+    # path to Hunpos Tag
 	echo -e "\nHUNPOS_TAGGER=\"$HOME/open-signs/vlibras-libs/aelius/bin/hunpos-tag\"" >> ~/.bashrc
 	echo -e "export HUNPOS_TAGGER" >> ~/.bashrc
 
@@ -68,6 +72,6 @@ read -r -p "Deseja criar as variáveis de ambiente? [Y/n] " response
 	echo -e "export PYTHONPATH\n" >> ~/.bashrc
 
 	echo -e "\n## Execute o seguinte comando para concluir:\n\n$ source ~/.bashrc\n"
- fi
+fi
 
 echo -e "### Instalação finalizada!"
